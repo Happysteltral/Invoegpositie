72a82c0ee15e1a55d5985586678958a23515dc95
Nicky Vergauwen <nickyvergauwen@gmail.com>
2025-01-29 09:07:11 +0100

    Versie voor logboekbericht met test

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 
 int searchInsert(int* nums, int numsSize, int target) {
-   //Vul hier jouw code in
-
+    int left = 0, right = numsSize - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
 }
 
 // Testfunctie
